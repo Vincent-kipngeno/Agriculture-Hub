b72773dfcb0444204af3e75f04a33fe871320fb3
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-05-19 20:46:26 +0300

    Add prototype for calculating total charge of product

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
-function order (type, quantity, delivery, location){
+function category (name){
+  this.name = name;
+  this.orders = [];
+}
+function order (type, quantity){
+  this.name = name;
   this.type = type;
   this.quantity = quamtity;
-  this.delivery = delivery;
-  this.location = location;
 }
+order.prototype.price = function () {
+  var productPrice;
+  switch (this.type) {
+    case "Eggs":
+      productPrice = 360;
+      break;
+    case "Beef":
+      productPrice = 300;
+      break;
+    case "Mutton":
+      productPrice = 350;
+      break;
+    case "Pork":
+      productPrice = 400;
+      break;
+    case "Wool":
+      productPrice = 200;
+      break;
+    default:
+      productPrice = 0;
+  }
+  totalCharge = productPrice * this.quqntity;
+};
