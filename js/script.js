eb8bafb8d5eb6497ec0cf5489c01c29e986c87f2
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-05-20 11:47:23 +0300

    Add style to css that hides form input for customers page animal-products module, Change the form class in customers.html for animalproducts module, change the form class and suggest another supply button class for farmers.html, add prototype for calculating farmers page price for products supplied

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 function category (name){
   this.name = name;
   this.orders = [];
@@ -8,7 +7,7 @@ function order (type, quantity){
   this.type = type;
   this.quantity = quantity;
 }
-order.prototype.price = function () {
+order.prototype.priceCustomers = function () {
   var productPrice;
   switch (this.type) {
     case "Eggs":
@@ -32,6 +31,30 @@ order.prototype.price = function () {
   totalCharge = productPrice * this.quantity;
   return totalCharge;
 };
+order.prototype.priceFarmers = function () {
+  var productPrice;
+  switch (this.type) {
+    case "Eggs":
+      productPrice = 300;
+      break;
+    case "Beef":
+      productPrice = 200;
+      break;
+    case "Mutton":
+      productPrice = 250;
+      break;
+    case "Pork":
+      productPrice = 300;
+      break;
+    case "Wool":
+      productPrice = 100;
+      break;
+    default:
+      productPrice = 0;
+  }
+  totalCharge = productPrice * this.quantity;
+  return totalCharge;
+};
 order.prototype.orderSummary = function () {
   return ("<ul>"+
           "<li> Type: "+ this.type +
@@ -39,6 +62,7 @@ order.prototype.orderSummary = function () {
            "<li> charge: "+ this.price() +
          +"</ul>");
 };
+
 function collector (categoryName){
   var type = $(".animal-products").val();
   var amount = $(".quantity").val();
@@ -50,6 +74,9 @@ function collector (categoryName){
 
 var allCategories = [];
 $(document).ready(function(){
+  $(".showForm1-customers").click(function(){
+    $(".form1-customers").toggle();
+  });
   $(".form1-customers").submit(function(event){
     event.preventDefault();
     var categoryName = "Animal Products: ";
@@ -63,4 +90,5 @@ $(document).ready(function(){
       $(".submit").show();
     });
   });
+  
 });
