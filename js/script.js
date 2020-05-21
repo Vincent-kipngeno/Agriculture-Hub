577fb9428cb302fd66b8ab79e64fff099f4e7163
ivine369 <ivinekemuma014@gmail.com>
2020-05-21 20:29:46 +0300

    replaced form1 with form7

--- js/script.js
+++ js/script.js
@@ -91,7 +91,7 @@ $(document).ready(function(){
     });
   });
   $(".showForm1-farmers").click(function(){
-    $(".form1-farmers").toggle();
+    $(".form7-farmers").toggle();
   });
   $(".form1-farmers").submit(function(event){
     event.preventDefault();
@@ -107,6 +107,35 @@ $(document).ready(function(){
     });
   });
 });
+
+order.prototype.priceCustomers = function () {
+  var productPrice;
+  switch (this.type) {
+    case "Kales":
+      productPrice = 2000;
+      break;
+    case "Cabbage":
+      productPrice = 1500;
+      break;
+    case "Spinach":
+      productPrice = 1200;
+      break;
+    case "Lettuce":
+      productPrice = 200;
+      break;
+    case "Cauliflower":
+      productPrice = 1500;
+      break;
+    case "coriander":
+      productPrice = 350;
+      break;
+    default:
+      productPrice = 0;
+  }
+  totalCharge = productPrice * this.quantity;
+  return totalCharge;
+};
+
 order.prototype.priceFarmers = function () {
   var productPrice;
   switch (this.type) {
@@ -134,6 +163,47 @@ order.prototype.priceFarmers = function () {
   totalCharge = productPrice * this.quantity;
   return totalCharge;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 order.prototype.orderSummary = function () {
   return ("<ul>"+
           "<li> Type: "+ this.type +
@@ -143,9 +213,9 @@ order.prototype.orderSummary = function () {
 };
 
 
-function collector (categoryName){
+function collectorVegetables (categoryName){
   var type = $(".vegetable-products").val();
-  var amount = $(".quantity").val();
+  var amount = parent($(".quantity").val());
   newOrder = new order (type, amount);
   productsType = new category(categoryName);
   productsType.orders.push(newOrder);
@@ -154,14 +224,14 @@ function collector (categoryName){
 
 var allCategories = [];
 $(document).ready(function(){
-  $(".showForm1-customers").click(function(){
-    $(".form1-customers").toggle();
+  $(".showForm7-customers").click(function(){
+    $(".form7-customers").toggle();
   });
-  $(".form1-customers").submit(function(event){
+  $(".form7-customers").submit(function(event){
     event.preventDefault();
     var categoryName = "Vegetable Products: ";
-    collector(categoryName);
-    $(".charge1-customers").append("<li>" + newOrder.type + ": Kshs" + newOrder.price() + "</li>");
+    collectorVegetables(categoryName);
+    $(".charge7-customers").append("<li>" + newOrder.type + ": Kshs" + newOrder.price() + "</li>");
     $(".another-order").show();
     $(".submit").hide();
     $(".another-order").off("click").on("click",function(){
@@ -170,14 +240,14 @@ $(document).ready(function(){
       $(".submit").show();
     });
   });
-  $(".showForm1-farmers").click(function(){
-    $(".form1-farmers").toggle();
+  $(".showForm7-farmers").click(function(){
+    $(".form7-farmers").toggle();
   });
-  $(".form1-farmers").submit(function(event){
+  $(".form7-farmers").submit(function(event){
     event.preventDefault();
     var categoryName = "Vegetable Products: ";
     collector(categoryName);
-    $(".charge1-farmers").append("<li>" + newOrder.type + ": Kshs" + newOrder.priceFarmers() + "</li>");
+    $(".charge7-farmers").append("<li>" + newOrder.type + ": Kshs" + newOrder.priceFarmers() + "</li>");
     $(".another-order").show();
     $(".submit").hide();
     $(".another-order").off("click").on("click",function(){
@@ -187,3 +257,25 @@ $(document).ready(function(){
     });
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
